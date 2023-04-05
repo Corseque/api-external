0db5a2eb5a0f64f261eb475c59f2e7e328b79972
Corseque <corseque@ya.ru>
2023-04-05 21:27:47 +0300

    api-manufacturer status enum to string

---
+++ src/main/java/ru/apiexternal/ApiExternalApplication.java
@@ -0,0 +1,13 @@
+package ru.apiexternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiExternalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/apiexternal/rest/ManufacturerRestController.java
@@ -0,0 +1,47 @@
+package ru.apiexternal.rest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.api.manufacturer.api.ManufacturerGateway;
+import ru.api.manufacturer.dto.ManufacturerDto;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/manufacturer")
+@Slf4j
+public class ManufacturerRestController {
+
+    private final ManufacturerGateway manufacturerGateway;
+
+    @GetMapping("/all")
+    public List<ManufacturerDto> getManufacturerList() {
+        return manufacturerGateway.getManufacturerList();
+    }
+
+    @GetMapping("/{manufacturerId}")
+    public ResponseEntity<?> getManufacturer(@PathVariable(name = "manufacturerId") Long id) {
+        return manufacturerGateway.getManufacturer(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addManufacturer(@Validated @RequestBody ManufacturerDto manufacturerDto) {
+        return manufacturerGateway.addManufacturer(manufacturerDto);
+    }
+
+    @PutMapping("/{manufacturerId}")
+    public ResponseEntity<?> updateManufacturer(@PathVariable(name = "manufacturerId") Long id,
+                                                @Validated @RequestBody ManufacturerDto manufacturerDto) {
+        return manufacturerGateway.updateManufacturer(id, manufacturerDto);
+    }
+
+    @DeleteMapping("/{manufacturerId}")
+    public void deleteById(@PathVariable(name = "manufacturerId") Long id) {
+        manufacturerGateway.deleteById(id);
+    }
+}
+
